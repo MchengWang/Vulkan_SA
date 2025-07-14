ff5a9ffd0189352e89d8a8f6377d20dfb68fbc3b
MchengWang <2734190606@qq.com>
2025-07-14 21:39:04 +0800

    validation Layers
    1、 understand what Validation Layers are.
    2、 how to use validation layers.
    3、 configure the message callback.

--- Vulkan_Simple_Application/source/base/main.cpp
+++ Vulkan_Simple_Application/source/base/main.cpp
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <string>
+#include <vector>
 
 import vulkan_hpp;
 
@@ -12,6 +13,17 @@ import vulkan_hpp;
 const uint32_t WIDTH = 1290;
 const uint32_t HEIGHT = 720;
 const std::string TITLE = "TRIANGLE";
+
+const std::vector validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif // NDEBUG
+
 
 class Triangle
 {
@@ -43,7 +55,8 @@ private:
 
 	void initVulkan()
 	{
-
+		createInstance();
+		setupDebugMessenger();
 	}
 
 	void createInstance()
@@ -59,28 +72,65 @@ private:
 			.apiVersion = vk::ApiVersion14
 		};
 
+		// 获取需要的表示层
+		std::vector<char const*> requiredLayers;
+		if (enableValidationLayers)
+			requiredLayers.assign(validationLayers.begin(), validationLayers.end());
+
+		// 检查 Vulkan 实现是否支持所需要的表示层
+		auto layerProperties = context.enumerateInstanceLayerProperties();
+		if (std::ranges::any_of(requiredLayers, [&layerProperties](auto const& requiredLayer)
+			{
+				return std::ranges::none_of(layerProperties, [requiredLayer](auto const& layerProperty)
+					{
+						return strcmp(layerProperty.layerName, requiredLayer) == 0;
+					});
+			}))
+			throw std::runtime_error("One or more required layers are not supported!");
+
 		// 从 GLFW 获取所需的实例扩展。
-		uint32_t glfwExtensionCount = 0;
-		const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		auto requiredExtensions = getRequiredExtensions();
 
 		// 检查 Vulkan 实现是否支持所需的 GLFW 扩展。
 		auto extensionProperties = context.enumerateInstanceExtensionProperties();
-		for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+		for (auto const& requiredExtension : requiredExtensions)
 		{
 			if (std::ranges::none_of(extensionProperties,
-				[glfwExtension = glfwExtensions[i]](auto const& extensionProperty)
-				{ return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
-				throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+				[requiredExtension](auto const& extensionProperty)
+				{ return strcmp(extensionProperty.extensionName, requiredExtension) == 0; }))
+				throw std::runtime_error("Required GLFW extension not supported: " + std::string(requiredExtension));
 		}
 
 		vk::InstanceCreateInfo createInfo{
 			.pApplicationInfo = &appInfo,
-			.enabledExtensionCount = glfwExtensionCount,
-			.ppEnabledExtensionNames = glfwExtensions
+			.enabledLayerCount = static_cast<uint32_t>(requiredLayers.size()),
+			.ppEnabledLayerNames = requiredLayers.data(),
+			.enabledExtensionCount = static_cast<uint32_t>(requiredExtensions.size()),
+			.ppEnabledExtensionNames = requiredExtensions.data(),
 		};
 
 		instance = vk::raii::Instance(context, createInfo);
 
+	}
+
+	void setupDebugMessenger()
+	{
+		if (!enableValidationLayers) return;
+
+		// 指定希望调用回传的所有类型的严重性
+		vk::DebugUtilsMessageSeverityFlagsEXT severityFlags(vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
+															vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+															vk::DebugUtilsMessageSeverityFlagBitsEXT::eError);
+		// 筛选通知回传的消息类型
+		vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags(vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+														   vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
+														   vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation);
+		vk::DebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfoEXT{
+			.messageSeverity = severityFlags,
+			.messageType = messageTypeFlags,
+			.pfnUserCallback = &debugCallback
+		};
+		debugMessenger = instance.createDebugUtilsMessengerEXT(debugUtilsMessengerCreateInfoEXT);
 	}
 
 	void mainLoop()
@@ -99,6 +149,29 @@ private:
 	}
 
 /**
+* helper functions
+*/
+private:
+	std::vector<const char*> getRequiredExtensions()
+	{
+		uint32_t glfwExtensionCount = 0;
+		auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		std::vector extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+		if (enableValidationLayers)
+			extensions.push_back(vk::EXTDebugUtilsExtensionName);
+
+		return extensions;
+	}
+
+	static VKAPI_ATTR vk::Bool32 VKAPI_CALL debugCallback(vk::DebugUtilsMessageSeverityFlagBitsEXT severty, vk::DebugUtilsMessageTypeFlagsEXT type, const vk::DebugUtilsMessengerCallbackDataEXT* pCallbackData, void*)
+	{
+		std::cerr << "validation layer: type " << to_string(type) << "msg" << pCallbackData->pMessage << std::endl;
+
+		return vk::False;
+	}
+
+/**
 * private variables (private functions calls only)
 */
 private:
@@ -106,6 +179,7 @@ private:
 
 	vk::raii::Context context;
 	vk::raii::Instance instance = nullptr;
+	vk::raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
 
 };
 
